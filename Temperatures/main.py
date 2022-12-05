2c761d70134efa63847f520c1aa467d9d6de9010
Emilio Mendoza <Ironislife39@gmail.com>
2022-12-05 08:43:20 -0500

    Made Temperature into TemperatureStats

--- Temperatures/main.py
+++ Temperatures/main.py
@@ -1,10 +1,16 @@
-def Temperatures():
+def TemperatureStats():
+    print("This program records the daily maximum temperatures for a week, and then outputs the average weekly high temperature.")
     days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     temps = []
     for day in days:
-        temps.append(input(f"Please enter the temperature for {day}: "))
+        temps.append(float(input(f"Please enter the temperature for {day}: ")))
     print()
     for day in range(len(days)):
-        print(f"Your daily high temperature for {days[day]} is {float(temps[day])}")
-
-Temperatures()
+        print(f"Your daily high temperature for {days[day]} is {temps[day]}")
+    print()
+    average = 0
+    for temp in temps:
+        average += temp
+    average /= len(temps)
+    print(f"The average weekly high temperature is: {round(average, 1)}")
+TemperatureStats()
